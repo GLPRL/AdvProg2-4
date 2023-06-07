14d6d84bb911009fdacfb0ecf49b312c3a4f960b
dekel98s <dekel98@gmail.com>
2023-06-07 18:23:32 +0300

    contacts xml looks good

--- app/src/main/java/com/example/advprog2_4/ContactsActivity.java
+++ app/src/main/java/com/example/advprog2_4/ContactsActivity.java
@@ -9,12 +9,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ContactsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
-
+        CircleImageView profilePicView = findViewById(R.id.profilePicView);
+        profilePicView.setImageResource(R.drawable.profile_pic_2);
         RecyclerView recyclerView = findViewById(R.id.recyclerContacts);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new ContactsAdapter(getApplicationContext(),generateContactList()));
@@ -22,21 +25,19 @@ public class ContactsActivity extends AppCompatActivity {
     }
     public List<Contact> generateContactList(){
         List<Contact> contactList = new ArrayList<Contact>();
-        contactList.add(new Contact("Dekel","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Naor","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Gal","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Dekel","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Naor","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Gal","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Dekel","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Naor","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Gal","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Dekel","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Naor","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Gal","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Dekel","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Naor","10/10/1999",R.drawable.baseline_account_circle_24));
-        contactList.add(new Contact("Gal","10/10/1999",R.drawable.baseline_account_circle_24));
+        contactList.add(new Contact("Dekel","10/10/1999",R.drawable.profile_pic_1));
+        contactList.add(new Contact("Naor","10/10/1999",R.drawable.profile_pic_1));
+        contactList.add(new Contact("Gal","10/10/1999",R.drawable.profile_pic_1));
+        contactList.add(new Contact("Dekel","10/10/1999",R.drawable.profile_pic_1));
+        contactList.add(new Contact("Naor","10/10/1999",R.drawable.profile_pic_1));
+        contactList.add(new Contact("Gal","10/10/1999",R.drawable.profile_pic_1));
+        contactList.add(new Contact("Dekel","10/10/1999",R.drawable.profile_pic_1));
+        contactList.add(new Contact("Naor","10/10/1999",R.drawable.profile_pic_1));
+        contactList.add(new Contact("Gal","10/10/1999",R.drawable.profile_pic_1));
+        contactList.add(new Contact("Dekel","10/10/1999",R.drawable.profile_pic_1));
+        contactList.add(new Contact("Naor","10/10/1999",R.drawable.profile_pic_1));
+        contactList.add(new Contact("Gal","10/10/1999",R.drawable.profile_pic_1));
+
         return contactList;
     }
 }
