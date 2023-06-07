28762ff462363850f272a749c7d65029c57c29cd
dekel98s <dekel98@gmail.com>
2023-06-07 23:31:21 +0300

    basic chat interaction need to improve but constarints are good!

--- app/src/main/java/com/example/advprog2_4/ChatActivity.java
+++ app/src/main/java/com/example/advprog2_4/ChatActivity.java
@@ -29,6 +29,20 @@ public class ChatActivity extends AppCompatActivity {
         messages.add(new Message("Hi", 0)); // Left-aligned message
         messages.add(new Message("How are you?", 1)); // Right-aligned message
         messages.add(new Message("I'm good, thanks!", 0)); // Left-aligned message
+        messages.add(new Message("Hello", 1)); // Right-aligned message
+        messages.add(new Message("Hi", 0)); // Left-aligned message
+        messages.add(new Message("How are you?", 1)); // Right-aligned message
+        messages.add(new Message("I'm good, thanks!", 0)); // Left-aligned message
+        messages.add(new Message("Hello", 1)); // Right-aligned message
+        messages.add(new Message("Hi", 0)); // Left-aligned message
+        messages.add(new Message("How are you?", 1)); // Right-aligned message
+        messages.add(new Message("I'm good, thanks!", 0)); // Left-aligned message
+        messages.add(new Message("Hello", 1)); // Right-aligned message
+        messages.add(new Message("Hi", 0)); // Left-aligned message
+        messages.add(new Message("How are you?", 1)); // Right-aligned message
+        messages.add(new Message("I'm good, thanks!", 0)); // Left-aligned message
+        messages.add(new Message("I'm good, thanks!", 0)); // Left-aligned message
+        messages.add(new Message("I'm good, thanks!", 0)); // Left-aligned message
 
 // Create the adapter and set it on the RecyclerView
         MessagesAdapter adapter = new MessagesAdapter(messages);
