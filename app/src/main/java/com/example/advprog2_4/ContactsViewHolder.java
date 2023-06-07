756d2a12c14196587a039bb2770b610fb45e5174
dekel98s <dekel98@gmail.com>
2023-06-07 18:43:59 +0300

    contacts change color on click

--- app/src/main/java/com/example/advprog2_4/ContactsViewHolder.java
+++ app/src/main/java/com/example/advprog2_4/ContactsViewHolder.java
@@ -1,17 +1,41 @@
 package com.example.advprog2_4;
+import android.graphics.Color;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ContactsViewHolder extends RecyclerView.ViewHolder {
     TextView date,displayName;
     ImageView profilePicView;
+    RelativeLayout relativeLayout;
 
     public ContactsViewHolder(View v) {
         super(v);
         profilePicView = v.findViewById(R.id.profilePicView);
         displayName = v.findViewById(R.id.displayName);
         date = v.findViewById(R.id.date);
+        relativeLayout= v.findViewById(R.id.contactRelativeLayout);
+
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Change background color to blue (#0000FF)
+                relativeLayout.setBackgroundResource(R.color.dark_white);
+
+                // Create a Handler and post a delayed runnable
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Revert the background color to its original state
+                        relativeLayout.setBackgroundResource(android.R.color.transparent);
+                    }
+                }, 200); // Delay for 0.5 seconds (500 milliseconds)
+            }
+        });
     }
 }
