b5c87b386d3888900f430800a794d0354bb7702e
dekel98s <dekel98@gmail.com>
2023-06-08 14:04:55 +0300

    can send message now

--- app/src/main/java/com/example/advprog2_4/ChatActivity.java
+++ app/src/main/java/com/example/advprog2_4/ChatActivity.java
@@ -3,27 +3,60 @@ package com.example.advprog2_4;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.button.MaterialButton;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
+    private MaterialButton sendButton;
+    private RecyclerView chatRecyclerView;
+    private EditText messageEditText;
+    private List<Message> messageList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
         TextView tvUser = findViewById(R.id.loggedUser);
         Intent intent = getIntent();
         String displayName = intent.getStringExtra("displayName");
         tvUser.setText(displayName);
-        RecyclerView recyclerView = findViewById(R.id.chatRecyclerView);
 
-// Create a list of messages
+        chatRecyclerView = findViewById(R.id.chatRecyclerView);
+        sendButton = findViewById(R.id.sendButton);
+        messageEditText = findViewById(R.id.messageEditText);
+
+        messageList = generateMessages();
+        MessagesAdapter adapter = new MessagesAdapter(messageList);
+        chatRecyclerView.setAdapter(adapter);
+        chatRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText = messageEditText.getText().toString().trim();
+                if (!messageText.isEmpty()) {
+                    Message newMessage = new Message(messageText, 1);
+                    messageList.add(newMessage);
+                    adapter.notifyItemInserted(messageList.size() - 1);
+                    chatRecyclerView.scrollToPosition(messageList.size() - 1);
+                    messageEditText.setText("");
+                }
+            }
+        });
+    }
+
+    public List<Message> generateMessages() {
         List<Message> messages = new ArrayList<>();
         messages.add(new Message("Hello", 1)); // Right-aligned message
         messages.add(new Message("Hi", 0)); // Left-aligned message
@@ -43,11 +76,6 @@ public class ChatActivity extends AppCompatActivity {
         messages.add(new Message("I'm good, thanks!", 0)); // Left-aligned message
         messages.add(new Message("I'm good, thanks!", 0)); // Left-aligned message
         messages.add(new Message("I'm good, thanks!", 0)); // Left-aligned message
-
-// Create the adapter and set it on the RecyclerView
-        MessagesAdapter adapter = new MessagesAdapter(messages);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
+        return messages;
     }
 }
