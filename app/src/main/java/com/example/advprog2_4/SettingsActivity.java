ae23116a2a5056a80081ed14c90dd362afef7579
dekel98s <dekel98@gmail.com>
2023-06-08 19:30:07 +0300

    changes to settings activity added back button

--- app/src/main/java/com/example/advprog2_4/SettingsActivity.java
+++ app/src/main/java/com/example/advprog2_4/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.advprog2_4;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,8 +9,11 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class SettingsActivity extends AppCompatActivity {
     Button btnSetServerAddress;
+    FloatingActionButton btnBack;
     Global global;
     EditText etServerAddress;
     @Override
@@ -27,6 +31,14 @@ public class SettingsActivity extends AppCompatActivity {
                 global.getInstance().setServerAddress(addr);
                 Toast.makeText(SettingsActivity.this, "Server address set successfully", Toast.LENGTH_SHORT).show();
             }
+        }
+    });
+    btnBack = findViewById(R.id.btnBack);
+    btnBack.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+            startActivity(intent);
         }
     });
     }
