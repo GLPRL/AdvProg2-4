34b725f11443bf59bce5cc104b030b6ae827f99d
NaorGavriel <117026659+NaorGavriel@users.noreply.github.com>
2023-06-14 16:26:52 +0300

    moved TokenAPI logic into onlick in mainactivity

--- app/src/main/java/com/example/advprog2_4/MainActivity.java
+++ app/src/main/java/com/example/advprog2_4/MainActivity.java
@@ -12,7 +12,17 @@ import android.widget.Toast;
 
 import com.example.advprog2_4.api.TokenAPI;
 import com.example.advprog2_4.api.UserAPI;
+import com.example.advprog2_4.api.WebServiceAPI;
+import com.example.advprog2_4.objects.UserIdAndPassword;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TokenAPI tokenAPI = new TokenAPI();
-        tokenAPI.post("ngasdas", "aaaaaaa1");
-        Log.i("TokenIs", Global.getInstance().getToken());
+        Global.getInstance().setToken("");
 
 
         //UserAPI userAPI = new UserAPI();
@@ -44,17 +51,47 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                // Define the navigation logic here
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
                 Intent intent = new Intent(MainActivity.this, ContactsActivity.class);
-                if(etUsername.getText().toString().equals("Test") && etPassword.getText().toString().equals("123")) {
-                    intent.putExtra("username", etUsername.getText().toString());
-                    startActivity(intent);
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Incorrect password and/or username", Toast.LENGTH_LONG).show();
-                    etUsername.setText("");
-                    etPassword.setText("");
-                }
+
+                Retrofit retrofit;
+                WebServiceAPI webServiceAPI;
+                Gson gson = new GsonBuilder()
+                        .setLenient()
+                        .create();
+
+                retrofit = new Retrofit.Builder()
+                        .baseUrl(MyApplication.context.getString(R.string.BaseUrl))
+                        .addConverterFactory(GsonConverterFactory.create(gson))
+                        .build();
+                webServiceAPI = retrofit.create(WebServiceAPI.class);
+
+                UserIdAndPassword userData = new UserIdAndPassword(username, password);
+
+
+                Call<String> call = webServiceAPI.postToken("application/json", "*/*" , userData);
+                call.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+
+                        String token = response.body();
+                        if (token == null) {
+                            Global.getInstance().setToken("");
+                            Toast.makeText(getApplicationContext(), "Incorrect password and/or username", Toast.LENGTH_LONG).show();
+                            etUsername.setText("");
+                            etPassword.setText("");
+                        } else {
+                            Global.getInstance().setToken(token);
+                            intent.putExtra("username", username);
+                            startActivity(intent);
+                        }
+
+                    }
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+                    }
+                });
             }
         });
 
@@ -75,6 +112,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         etUsername.setText("");
         etPassword.setText("");
+        Global.getInstance().setToken("");
     }
 
 }
