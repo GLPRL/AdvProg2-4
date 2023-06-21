93e61d9a9b3408e91c9ef44d4089e1a4eb752e1b
galpearl <galpearl24@gmail.com>
2023-06-21 22:00:02 +0300

    black magic and it works.

--- app/src/main/java/com/example/advprog2_4/ChatDao.java
+++ app/src/main/java/com/example/advprog2_4/ChatDao.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
@@ -19,7 +20,7 @@ public interface ChatDao {
     @Query("SELECT * FROM convertedchat WHERE id = :id")
     ConvertedChat get(int id);
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(ConvertedChat... convertedChats);
 
     @Update
