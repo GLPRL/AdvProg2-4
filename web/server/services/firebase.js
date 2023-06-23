a967fd1e3807651113e0574053caa4d07bcdab5f
galpearl <galpearl24@gmail.com>
2023-06-23 13:09:11 +0300

    firebase.js : find by schema in db of users&tokens and send message.

--- web/server/services/firebase.js
+++ web/server/services/firebase.js
@@ -1,20 +1,28 @@
 import {getMessaging} from "firebase-admin/messaging";
-import {response} from "express";
 
-function getFBToken(srcUser) {
+const fbToken = require("../models/firebase")
+
+const sendMessage = async (srcUser, destUser) => {
     //Need to iterate over users, grab the FB token from a field, build a message with the sender's name.
     //fbtoken = ???;
     //TODO
+    const firebaseCollection = mongoose.model("fbtokens", fbToken.schema, "fbtokens");
+    const user = await firebaseCollection.findOne({username: destUser}).exec();
+    if (user == null || user.token == null) {           //user not found or not android user
+        return
+    }
+
     const message = {
         data: {
-            user: srcUser
+            source: srcUser
         },
-        token: fbtoken
+        token: user.token
     };
+
 
     getMessaging().send(message)
         .then((response) => {
-        console.log("sent message");
+            console.log("sent message");
         })
         .catch((error) => {
             console.log("error sending ", error);
